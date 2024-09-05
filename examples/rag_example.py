6d9a48b058bf21873ec6836d4cd6dbcad78a17c3
pritam-dey3 <pritam.pritamdey.984@gmail.com>
2024-09-05 14:52:24 +0530

    Update rag example + automated docs deployment

--- examples/rag_example.py
+++ examples/rag_example.py
@@ -1,88 +1,83 @@
-# We will use the RAG model to generate a prompt for a given input and then use a similarity retriever to find similar sentences from a dataset.
-# This example uses the `SentenceTransformer` library to extract embedding vectors from the sentences.
-
 import asyncio
 
 import numpy as np
-import pandas as pd
-from interact import handler
+from openai import OpenAI
+
+from interact import HandlerChain, Message, handler
 from interact.handlers import OpenAiLLM, SimilarityRetriever
 from interact.retrieval import SimpleRecord
 from interact.retrieval.faiss import FaissIndexDB
-from sentence_transformers import SentenceTransformer
+from functools import partial
+
+client = OpenAI()
 
 
-def get_sentences(use_urls=False):
-    if use_urls:
-        from io import StringIO
+# an example dataset to demonstrate rag
+article = """In 2032, a dog named Timmy made history by becoming the first canine to land on Mars. At the age of 5, Timmy was specially selected for the mission due to his calm temperament and ability to adapt to new environments.
 
-        import requests
+Timmy traveled aboard the SpaceX Falcon X7, a rocket designed for deep space exploration. The spacecraft featured advanced life support systems and a speed of 22,000 miles per hour, ensuring Timmy’s safe journey to the red planet.
 
-        urls = [
-            "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/semeval-sts/2012/MSRpar.train.tsv",
-            "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/semeval-sts/2012/MSRpar.test.tsv",
-            "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/semeval-sts/2012/OnWN.test.tsv",
-            "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/semeval-sts/2013/OnWN.test.tsv",
-            "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/semeval-sts/2014/OnWN.test.tsv",
-            "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/semeval-sts/2014/images.test.tsv",
-            "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/semeval-sts/2015/images.test.tsv",
-        ]
+The mission took approximately seven months, and Timmy spent most of the trip in a custom-built habitat. This habitat included a climate-controlled sleeping area and robotic arms for providing food and water.
 
-        sentences = []
-        # each of these dataset have the same structure, so we loop through each creating our sentences data
-        url_datasets = []
-        for url in urls:
-            res = requests.get(url)
-            # extract to dataframe
-            url_data = pd.read_csv(
-                StringIO(res.text), sep="\t", header=None, on_bad_lines="warn"
-            )
-            url_datasets.append(url_data)
-        data = pd.concat(url_datasets)
-        data.to_csv("dummy_strings.csv", index=False)
+Upon landing on Mars in February 2033, Timmy became an international sensation, with scientists closely monitoring his behavior in the planet's 38% lower gravity. Equipped with a lightweight, pressurized dog suit, Timmy was able to explore the Martian surface safely, with built-in oxygen tanks, temperature regulation, and radiation shielding to protect him from the planet’s extreme conditions.
 
-    data = pd.read_csv("dummy_strings.csv")
-    sentences = data["1"].tolist()
-    sentences.extend(data["2"].tolist())
-    sentences = list(set(sentences))
-    return sentences
+On March 12, 2033, just a month after his landing, Timmy completed his first full exploration of a Martian crater, gathering data on how living beings adapt to the environment. His successful adaptation to Mars led NASA to announce plans to involve service animals in human missions by 2035."""
 
+paragraphs = article.split("\n\n")
 
-# create a dataset of sentences
-sentences = get_sentences(use_urls=True)
-print(f"Number of sentences: {len(sentences)}")
-dataset = [SimpleRecord(s) for s in sentences if isinstance(s, str)]
-
-# load encoder model
-model = SentenceTransformer("nomic-ai/nomic-embed-text-v1", trust_remote_code=True)
+dataset = [SimpleRecord(s) for s in paragraphs if isinstance(s, str)]
 
 
 def encode(texts: list[str], dim: int = 512) -> np.ndarray:
-    return model.encode(texts, convert_to_numpy=True)[:, :dim]  # type: ignore
+    response = client.embeddings.create(input=texts, model="text-embedding-3-small")
+    emb_matrix = np.ndarray((0, dim))
+    for resp_emb in response.data:
+        emb_matrix = np.r_[
+            emb_matrix, np.array(resp_emb.embedding[:dim])[np.newaxis, :]
+        ]
+    return emb_matrix
 
 
-# create faiss index
-index_db = FaissIndexDB(
-    (512, "IDMap,IVF30,PQ8"), dataset=dataset, encoder=encode, train=True
-)
-
-
-# define pipeline
 @handler
-async def create_prompt(msg, chain):
-    return f"Describe how do you recognize {msg} without mentioning it's name in one sentence."
+async def enhance_query(msg: Message, chain: HandlerChain):
+    chain.variables["query"] = msg
+    return (
+        "The following query is about a dog named Timmy who went to Mars. Rewrite the query to make it more verbose and easier to understand in one sentence.\n"
+        f"Original query: {msg}"
+        "Only rewrite the query in one sentence. Do not include any additional information, salutations, or sign-offs."
+    )
 
 
-pipe = (
-    create_prompt
-    >> OpenAiLLM()
-    >> SimilarityRetriever(index_db, k=3, join_policy="\n----x----\n")
-)
+@handler
+async def answer(msg: Message, chain: HandlerChain):
+    query = chain.variables["query"]
+    prompt = (
+        "Answer the given query from the context given below. Do not include any information that is not present in the context.\n"
+        f"--- Context start---\n{msg}\n--- Context end ---\n\n"
+        f"Query: {query}"
+    )
+    return prompt
 
 
-if __name__ == "__main__":
-    res = asyncio.run(pipe("a bmw car"))
-    print(res, end="\n\n")
+def main(dim=512):
+    index_db = FaissIndexDB(
+        (dim, "IDMap,Flat"), dataset=dataset, encoder=partial(encode, dim=dim),
+        train=True
+    )
+
+    pipe = (
+        enhance_query
+        >> OpenAiLLM()
+        >> SimilarityRetriever(index_db, k=2, join_policy="\n----x----\n")
+        >> answer
+        >> OpenAiLLM()
+    )
+    res = asyncio.run(pipe("When did Timmy land on Mars?"))
+    print(res)
 
     for msg in pipe.history:
         print(f"{msg.sender}:\n{msg.primary}\n")
+
+
+if __name__ == "__main__":
+    main()
